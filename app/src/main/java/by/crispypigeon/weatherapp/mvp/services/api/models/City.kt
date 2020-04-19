f211b5efc1bfb33a8498206e41c612bd5498ffc6
Sarafanoff Dmitriy <d.sarafanoff@gmail.com>
2020-04-19 21:05:32 +0200

    Debug Commit 3 - Models for weather Api added.

---
+++ app/src/main/java/by/crispypigeon/weatherapp/mvp/services/api/models/City.kt
@@ -0,0 +1,12 @@
+import com.google.gson.annotations.SerializedName
+
+data class City (
+
+	@SerializedName("id") val id : Int,
+	@SerializedName("name") val name : String,
+	@SerializedName("coord") val coord : Coord,
+	@SerializedName("country") val country : String,
+	@SerializedName("timezone") val timezone : Int,
+	@SerializedName("sunrise") val sunrise : Int,
+	@SerializedName("sunset") val sunset : Int
+)
